5462c1906289e63df00e5bc87e2e2c0115f4348b
ENGENUITY\cmarsh <cmarsh@engenuity.com>
2018-10-22 16:07:08 -0700

    Added all pages except schedule and settings, dynamic map images, and alarm rows.

---
+++ README.txt
@@ -0,0 +1,17 @@
+ASU Energy Management DEMO 
+
+  HOW TO RUN:
+	1. Right click "Overview.html" inside the current directory.
+	2. Hover over "Open with..."
+	    2.1. Select your favorite web browser (tested on latest Chrome and Firefox releases)
+	3. You will now be at the "Overview" page, which should be familiar looking.
+
+
+  WHAT WORKS: 
+      - Opening/closing location dropdowns will display the image of that general location's map
+      - Clicking on the view location (magnifying glass) will take you to a generic dashboard
+      - Each tab on the dashboard is able to be visited (but AV and Shades are blank), excluding schedule and settings.
+      - The logout button will take you back to the overview from any dashboard page.
+      - Buildings with critical alarms are highlighted red, and buildings with caution alarms are highlighted yellow/orange.
+      - Screenshots of each tab's "page" are displayed when you visit the tab, and the active tab is highlighted.
+
--- js/showLocations.js
+++ js/showLocations.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
+  var brandMap = document.getElementById("brandmap"); 
+  var imageSrc = document.getElementById("map-image").src;    
+    
   document.querySelectorAll('.tier1bar').forEach(function (bar) {
     var toggler = bar.querySelector('span.open')
     if (!toggler) return
@@ -6,8 +9,49 @@ document.addEventListener('DOMContentLoaded', function () {
     var contentWrapper = bar.querySelector('.tier2bars')
     if (!contentWrapper) return
     
+    //var imageSrc = brandMap.firstElementChild.src;
+    //var imageSrc = document.getElementById("map-image").src;
+    //if (!imageSrc) return
+    
     toggler.onclick = function () {
-      contentWrapper.classList.toggle('show')
+        contentWrapper.classList.toggle('show')
+        var location = bar.getElementsByClassName("brandname")[0].firstChild.nextSibling.innerHTML;
+        //console.log(brandMap.firstElementChild.src);
+        
+        switch(location) {
+            case "Tempe":
+                brandMap.firstElementChild.src = "file:///C:/ChameleonClone/images/tempemap.png";
+                //console.log("Tempe case");
+                break;
+            case "West":
+                brandMap.firstElementChild.src = "file:///C:/ChameleonClone/images/westmap.png";
+                //console.log("West case");
+                break;
+            case "Polytechnic":
+                brandMap.firstElementChild.src = "file:///C:/ChameleonClone/images/testmap.png";
+                //console.log("Poly case");
+                break;
+            case "Downtown Phoenix":
+                brandMap.firstElementChild.src = "file:///C:/ChameleonClone/images/phxmap.png";
+                //console.log("Downtown Phoenix case");
+                break;
+            case "Lake Havasu":
+                brandMap.firstElementChild.src = "file:///C:/ChameleonClone/images/havasumap.png";
+                //console.log("Lake Havasu case");
+                break;
+            case "Skysong":
+                brandMap.firstElementChild.src = "file:///C:/ChameleonClone/images/skysongmap.png";
+                //console.log("Skysong case");
+                break;
+            case "Research Park":
+                brandMap.firstElementChild.src = "file:///C:/ChameleonClone/images/researchmap.png";
+                //console.log("Research Park case");
+                break;  
+            default:
+                brandMap.firstElementChild.src = "file:///C:/ChameleonClone/images/testmap.png";
+                //console.log("default case");
+        }
+    
     }
   })
 })
